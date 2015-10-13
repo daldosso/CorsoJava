53485b899e72013399fe437ca00ec94eee3eb4ae
Alberto Dal Dosso <daldosso@gmail.com>
2015-10-13 19:15:46 +0200

    Aggiunto test IF

---
+++ src/adaldosso/HelloIf.java
@@ -0,0 +1,20 @@
+package adaldosso;
+
+/**
+ *
+ * @author Alberto Dal Dosso
+ */
+public class HelloIf {
+    
+    public static void main(String[] args) {
+        int operatore1 = 10;
+        int operatore2 = 20;
+        
+        if (operatore1 == operatore2) {
+            System.out.println("uguali");
+        } else {
+            System.out.println("diversi");
+        }
+    }
+
+}
