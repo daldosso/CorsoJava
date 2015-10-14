14bada46cc03ab9b4bf1b19f45c9f90814042507
Alberto Dal Dosso <daldosso@gmail.com>
2015-10-14 15:24:08 +0200

    Operatore condizionale

---
+++ src/adaldosso/HelloCondizionale.java
@@ -0,0 +1,14 @@
+package adaldosso;
+
+/**
+ *
+ * @author Alberto Dal Dosso
+ */
+public class HelloCondizionale {
+    
+    public static void main(String[] args) {
+        String ris = 10 < 20 ? "Vero" : "Falso";
+        System.out.println(ris);
+    }
+
+}
